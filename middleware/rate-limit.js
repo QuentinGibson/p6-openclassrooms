f45e0b875a06cf41790b5fc52b92efc461b63e56
QuentinGibson <qgibson3@students.kennesaw.edu>
2022-04-05 22:46:51 -0400

    add rate limit middleware for signups

---
+++ middleware/rate-limit.js
@@ -0,0 +1,21 @@
+const opts = {
+  points: 6, // 6 points
+  duration: 1, // Per second
+};
+
+const rateLimiter = new RateLimiterMemory(opts);
+
+module.exports = (req, res, next) => {
+  rateLimiter
+    .consume(userId, 2) // consume 2 points
+    .then((rateLimiterRes) => {
+      // 2 points consumed
+      next();
+    })
+    .catch((rateLimiterRes) => {
+      // Not enough points to consume
+      return res.status(400).json({
+        error: "Rate Limit Reached.",
+      });
+    });
+};
