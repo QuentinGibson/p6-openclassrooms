2b8517d70292d9b4bb718523739a035adfa8d95a
QuentinGibson <qgibson3@students.kennesaw.edu>
2022-04-06 01:13:17 -0400

    fix typeo

--- controllers/auth.js
+++ controllers/auth.js
@@ -45,9 +45,13 @@ exports.login = (req, res, next) => {
               error: new Error("Incorrect password!"),
             });
           }
-          const token = jwt.sign({ userId: user.id }, process.env.TOKENSECRET, {
-            expiresIn: "24h",
-          });
+          const token = jwt.sign(
+            { userId: user._id },
+            process.env.TOKENSECRET,
+            {
+              expiresIn: "24h",
+            }
+          );
           res.status(200).json({
             userId: user._id,
             token: token,
