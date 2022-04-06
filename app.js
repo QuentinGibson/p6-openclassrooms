6775c033df7a8691d4249b36bf2ac9b71d7ce093
QuentinGibson <qgibson3@students.kennesaw.edu>
2022-04-05 23:46:54 -0400

    add static route for images

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const helmet = require("helmet");
 const errorhandler = require("errorhandler");
+const path = require("path");
 const app = express();
 app.use(helmet());
+app.use("/images", express.static(path.join(__dirname, "images")));
 if (process.env.NODE_ENV === "development") {
   // only use in development
   app.use(errorhandler());
