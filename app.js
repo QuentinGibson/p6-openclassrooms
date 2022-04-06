fb2341eb740017b4ad93d39f33c0845c75051b1f
QuentinGibson <qgibson3@students.kennesaw.edu>
2022-04-06 00:51:05 -0400

    add routes to app

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const helmet = require("helmet");
 const errorhandler = require("errorhandler");
+const authRoutes = require("./routes/auth");
+const sauceRoutes = require("./routes/sauce");
 const path = require("path");
 const app = express();
 app.use(helmet());
+app.use("/api/auth", authRoutes);
+app.use("/api/sauce", sauceRoutes);
 app.use("/images", express.static(path.join(__dirname, "images")));
 if (process.env.NODE_ENV === "development") {
   // only use in development
