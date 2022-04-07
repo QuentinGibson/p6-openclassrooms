519fb08bf292409f92c8dfd7d56ee099e1d5ce65
QuentinGibson <qgibson3@students.kennesaw.edu>
2022-04-07 17:10:55 -0400

    fix bug on delete route for sauce

--- controllers/sauceController/deleteSauce.js
+++ controllers/sauceController/deleteSauce.js
@@ -1,13 +1,17 @@
 const Sauce = require("../../models/sauce");
+const jwt = require("jsonwebtoken");
 exports.deleteSauce = (req, res, next) => {
-  Sauce.findByIdAndRemove({ _id: req.params.id })
+  const token = req.headers.authorization.split(" ")[1];
+  const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
+  const { userId } = decodedToken;
+  Sauce.findByIdAndRemove(req.params.id)
     .then((sauce) => {
       if (!sauce) {
         return res.status(404).json({
           error: new Error("Sauce not found"),
         });
       }
-      if (sauce.userId !== req.auth.userId) {
+      if (sauce.userId !== userId) {
         return res.status(404).json({
           error: new Error("Sauce not found"),
         });
