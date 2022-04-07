c46e878f207825905b2b97f842389f0f2b568b1a
QuentinGibson <qgibson3@students.kennesaw.edu>
2022-04-07 17:11:04 -0400

    fix bugs for update route for sauce

--- controllers/sauceController/updateLike.js
+++ controllers/sauceController/updateLike.js
@@ -6,79 +6,68 @@ dotenv.config();
 exports.updateLike = (req, res, next) => {
   const token = req.headers.authorization.split(" ")[1];
   const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
-  const userId = decodedToken;
-  let currentSauce = Sauce.find({ _id: req.params._id });
-  let sauce;
-  if (currentSauce.usersLiked.includes(userId)) {
-    if (req.params.like === 1) {
+  const { userId } = decodedToken;
+  Sauce.findById({ _id: req.params.id }).then((currentSauce) => {
+    if (!currentSauce) {
       return res.status(400).json({
-        message: "You already liked this sauce",
+        message: "Could not find sauce ",
       });
-    } else if (req.params.like === -1) {
-      sauce = new Sauce(
-        { _id: req.params._id },
-        {
-          $inc: { dislike: 1 },
-          $inc: { like: -1 },
+    }
+    let update;
+    if (currentSauce.usersLiked.includes(userId)) {
+      if (req.body.like === 1) {
+        return res.status(400).json({
+          message: "You already liked this sauce",
+        });
+      } else if (req.body.like === -1) {
+        update = {
+          $inc: { dislikes: 1 },
+          $inc: { likes: -1 },
           $pull: { usersLiked: userId },
           $push: { usersDisliked: userId },
-        }
-      );
-    } else {
-      sauce = new Sauce(
-        { _id: req.params._id },
-        {
-          $inc: { like: -1 },
+        };
+      } else {
+        update = {
+          $inc: { likes: -1 },
           $pull: { usersLiked: userId },
-        }
-      );
-    }
-  } else if (currentSauce.usersDisliked.includes(userId)) {
-    if (req.params.like === 1) {
-      sauce = new Sauce(
-        { _id: req.params._id },
-        {
+        };
+      }
+    } else if (currentSauce.usersDisliked.includes(userId)) {
+      if (req.body.like === 1) {
+        update = {
           $inc: { dislike: -1 },
-          $inc: { like: 1 },
+          $inc: { likes: 1 },
           $push: { usersLiked: userId },
           $pull: { usersDisliked: userId },
-        }
-      );
-    } else if (req.params.like === -1) {
-      res.status(400).json({
-        message: "You already disliked this sauce",
-      });
+        };
+      } else if (req.body.like === -1) {
+        res.status(400).json({
+          message: "You already disliked this sauce",
+        });
+      } else {
+        update = {
+          $inc: { dislikes: -1 },
+          $pull: { usersDisliked: userId },
+        };
+      }
     } else {
-      sauce = new Sauce(
-        { _id: req.params._id },
-        {
-          $inc: { dislike: -1 },
-          $pull: { usersDisliked: userId },
-        }
-      );
-    }
-  } else {
-    if (req.params.like === 1) {
-      sauce = new Sauce(
-        { _id: req.params._id },
-        {
-          $inc: { like: 1 },
+      if (req.body.like === 1) {
+        update = {
+          $inc: { likes: 1 },
           $push: { usersLiked: userId },
-        }
-      );
-    } else if (req.params.like === -1) {
-      sauce = new Sauce(
-        { _id: req.params._id },
-        {
-          $inc: { dislike: 1 },
+        };
+      } else if (req.body.like === -1) {
+        update = {
+          $inc: { dislikes: 1 },
           $push: { usersDisliked: userId },
-        }
-      );
-    } else {
-      return res.stauts(400).json({ message: "Nothing happned" });
+        };
+      } else {
+        return res.status(400).json({ message: "There was an error" });
+      }
     }
-  }
-  Sauce.findByIdAndUpdate({ _id: req.params._id }, sauce).then((sauce) => {
-    res.status(200).json({ userId: userId, like: sauce.likes });
+    Sauce.updateOne({ _id: req.params.id }, update).then((sauce) => {
+      console.log(sauce);
+      res.status(200).json({ userId: sauce.userId, like: sauce.likes });
+    });
   });
 };
