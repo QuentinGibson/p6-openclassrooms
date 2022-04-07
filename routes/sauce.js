d436357af6d0bf594e5a9d3a85e886160221228b
QuentinGibson <qgibson3@students.kennesaw.edu>
2022-04-07 17:10:43 -0400

    fix paths routes for sauce

--- routes/sauce.js
+++ routes/sauce.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const multer = require("../middleware/multer-config");
+const auth = require("../middleware/auth");
 const { createSauce } = require("../controllers/sauceController/createSauce");
 const { deleteSauce } = require("../controllers/sauceController/deleteSauce");
 const { getAllSauces } = require("../controllers/sauceController/getAllSauces");
 const { getSauce } = require("../controllers/sauceController/getSauce");
 const { updateSauce } = require("../controllers/sauceController/updateSauce");
-const auth = require("../middleware/auth");
+const { updateLike } = require("../controllers/sauceController/updateLike");
 
 const router = express.Router();
 
 router.get("/", auth, getAllSauces);
 router.get("/:id", auth, getSauce);
 router.post("/", auth, multer, createSauce);
-router.post("/:id/likes", auth, updateSauce);
+router.post("/:id/like", auth, updateLike);
 router.put("/:id", auth, multer, updateSauce);
 router.delete("/:id", auth, deleteSauce);
 
