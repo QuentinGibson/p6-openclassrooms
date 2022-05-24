5a104d30d9b1a8bcf1c082c62704ef7557771ba0
Quentin Gibson <quentingibson94@gmail.com>
2022-05-24 17:48:33 -0400

    refactor: auth for signup and login

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,33 +5,34 @@ const jwt = require("jsonwebtoken");
 exports.signup = (req, res, next) => {
   bcrypt
     .hash(req.body.password, 10)
-    .then((hash) => {
+    .then(async (hash) => {
       const user = new User({
         email: req.body.email,
         password: hash,
       });
-      user
-        .save()
-        .then(() => {
-          res.status(201).json({
-            message: "User added successfully",
-          });
-        })
-        .catch((error) => {
-          console.error("Could not save new user: " + error);
-          res.status(500).json({
-            error,
-          });
-        });
+      await User.findOne({ email: req.body.email }).then(async (_user) => {
+        if (_user) {
+          console.log(`User already exists`);
+        } else {
+          await user.save();
+        }
+      });
+    })
+    .then(() => {
+      console.log("User created successfully");
+      res.status(201).json({
+        message: "User created successfully",
+      });
     })
     .catch((error) => {
+      console.log("Could not save new user: " + error);
       res.status(500).json({
-        error,
+        error: error,
       });
     });
 };
-exports.login = (req, res, next) => {
-  User.findOne({ email: req.body.email })
+exports.login = async (req, res, next) => {
+  await User.findOne({ email: req.body.email })
     .then((user) => {
       if (!user) {
         return res.status(401).json({
@@ -42,29 +43,34 @@ exports.login = (req, res, next) => {
         .compare(req.body.password, user.password)
         .then((valid) => {
           if (!valid) {
-            return res.status(401).json({
+            console.log("Wrong password");
+            res.status(401).json({
               error: new Error("Incorrect password!"),
             });
+          } else {
+            const token = jwt.sign(
+              { userId: user._id },
+              process.env.TOKEN_SECRET,
+              {
+                expiresIn: "24h",
+              }
+            );
+            console.log(`Token session given out`);
+            res.status(200).json({
+              userId: user._id,
+              token,
+            });
           }
-          const token = jwt.sign(
-            { userId: user._id },
-            process.env.TOKEN_SECRET,
-            {
-              expiresIn: "24h",
-            }
-          );
-          res.status(200).json({
-            userId: user._id,
-            token: token,
-          });
         })
         .catch((error) => {
+          console.log(error);
           res.status(500).json({
             error,
           });
         });
     })
     .catch((error) => {
+      console.log(`outer: ${error}`);
       res.status(500).json({
         error,
       });
